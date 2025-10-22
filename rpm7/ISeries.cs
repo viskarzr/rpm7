74ebd49428ad0d70c4814f557290b5deb79c9133
ИСП-31 <-31@LAB17-05>
2025-10-22 12:06:24 +0300

    Добавьте файлы проекта.

---
+++ rpm7/ISeries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpm7
+{
+    internal interface ISeries
+    {
+        int Next {  get; }
+        int GetNext();
+        void Reset();
+        void SetStart(int x);
+    }
+}
---
+++ rpm7/Series.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpm7
+{
+     class Series : ISeries
+    {
+        int start;
+        int value;
+        public Series()
+        {
+            start = 0;
+            value = 0;
+        }
+
+        public int Next
+        {
+            get {  return GetNext(); }
+        }
+
+        public int GetNext()
+        {
+            value += 2;
+            return value;
+        }
+        public void Reset()
+        {
+            value = start;
+        }
+
+        public void SetStart(int x)
+        {
+            start = x;
+            value = start;
+        }
+    }
+}
